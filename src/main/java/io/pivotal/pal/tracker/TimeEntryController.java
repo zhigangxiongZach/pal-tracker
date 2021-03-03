ea043da8bdb76ef916508ec3cff84861c9e2829d
zhigangxiongZach <zxiong1@yahoo.com>
2021-03-03 17:34:50 +0000

    add annotation for Rest API

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -2,9 +2,11 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class TimeEntryController {
     private static TimeEntry timeEntry;
     private final TimeEntryRepository timeEntryRepository;
@@ -13,12 +15,14 @@ public class TimeEntryController {
         this.timeEntryRepository = timeEntryRepository;
     }
 
-    public ResponseEntity<TimeEntry> create(TimeEntry timeEntryToCreate) {
+    @PostMapping("/time-entries")
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
         TimeEntry timeEntryRecord = this.timeEntryRepository.create(timeEntryToCreate);
         return ResponseEntity.created(null).body(timeEntryRecord);
     }
 
-    public ResponseEntity<TimeEntry> read(long id) {
+    @GetMapping("/time-entries/{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long id) {
         TimeEntry timeEntry = this.timeEntryRepository.find(id);
         if (timeEntry == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -26,17 +30,20 @@ public class TimeEntryController {
         return ResponseEntity.ok(timeEntry);
     }
 
-    public ResponseEntity<Void> delete(long id) {
+    @DeleteMapping("/time-entries/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         this.timeEntryRepository.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
+    @GetMapping("/time-entries")
     public ResponseEntity<List<TimeEntry>> list() {
         List<TimeEntry> timeEntryList = this.timeEntryRepository.list();
         return ResponseEntity.ok(timeEntryList);
     }
 
-    public ResponseEntity<TimeEntry> update(long id, TimeEntry timeEntryToUpdate) {
+    @PutMapping("/time-entries/{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long id, @RequestBody TimeEntry timeEntryToUpdate) {
         TimeEntry timeEntry = this.timeEntryRepository.update(id, timeEntryToUpdate);
         if (timeEntry != null) {
             return ResponseEntity.ok(timeEntry);
